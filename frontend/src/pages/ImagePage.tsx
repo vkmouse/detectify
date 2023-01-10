63573bb733b1421b09cb449d9419bcaa5beb2fa8
vkmouse <knight31241@gmail.com>
2023-01-10 19:50:49 +0800

    feat: use env variable to set api url

--- frontend/src/pages/ImagePage.tsx
+++ frontend/src/pages/ImagePage.tsx
@@ -1,5 +1,43 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import api from '../api/api';
+
+const ImageUploader = styled.input.attrs({
+  type: 'file',
+  accept: '.jpg,.jpeg,.png',
+})``;
+
 const ImagePage = () => {
-  return <>image page</>;
+  const [message, setMessage] = useState('');
+  const [file, setFile] = useState<File | undefined>();
+
+  const handleUpload = async () => {
+    if (file) {
+      const presignedURL = await api.createUpload(file.name);
+      const success = await api.uploadStorageService(presignedURL, file);
+      console.log(`Upload ${file.name} ${success}`);
+      console.log(file.type);
+    }
+  };
+
+  return (
+    <>
+      <h1>發表一篇圖文</h1>
+      <div>
+        <span>請輸入文字: </span>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <span>請選擇檔案: </span>
+        <ImageUploader onChange={(e) => setFile(e.target.files?.[0])} />
+      </div>
+      <button onClick={handleUpload}>送出</button>
+    </>
+  );
 };
 
 export default ImagePage;
--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -27,5 +28,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './public/index.html'),
     }),
+    new Dotenv(),
   ],
 };
