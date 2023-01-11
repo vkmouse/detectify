ede058b5f5657b4635b0f5b9c4b748e28a19f53b
vkmouse <knight31241@gmail.com>
2023-01-11 16:15:31 +0800

    chore: add gorm and config variable

--- backend/config/config.go
+++ backend/config/config.go
@@ -11,6 +11,12 @@ var (
 	HttpPort    string
 	AllowOrigin string
 
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassword string
+	DbName     string
+
 	R2BucketName      string
 	R2AccountId       string
 	R2AccessKeyId     string
@@ -21,6 +27,12 @@ func init() {
 	Mode = os.Getenv("MODE")
 	HttpPort = os.Getenv("HTTP_PORT")
 	AllowOrigin = os.Getenv("ALLOWED_CORS_ORIGIN")
+
+	DbHost = os.Getenv("DB_HOST")
+	DbPort = os.Getenv("DB_PORT")
+	DbUser = os.Getenv("DB_USER")
+	DbPassword = os.Getenv("DB_PASSWORD")
+	DbName = os.Getenv("DB_NAME")
 
 	R2BucketName = os.Getenv("R2_BUCKET_NAME")
 	R2AccountId = os.Getenv("R2_ACCOUNT_ID")
