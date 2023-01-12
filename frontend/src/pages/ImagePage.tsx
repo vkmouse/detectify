62c7da4fbfb95361f172ed9ab1662d2c9b8d6286
vkmouse <knight31241@gmail.com>
2023-01-12 16:03:51 +0800

    feat: use react query to fetch

--- frontend/src/pages/ImagePage.tsx
+++ frontend/src/pages/ImagePage.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
 import api, { Message } from '../api/api';
 
@@ -7,29 +8,52 @@ const ImageUploader = styled.input.attrs({
   accept: '.jpg,.jpeg,.png',
 })``;
 
+export const CenterCropped = styled.img`
+  object-fit: cover;
+  object-position: center;
+`;
+
 const ImagePage = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [content, setContent] = useState('');
-  const [file, setFile] = useState<File | undefined>();
+  const selectedFilePath = useRef('');
+  const [selectedFile, setSelectedFile] = useState<File | undefined>();
 
-  const updateMessages = async () => {
-    const messages = await api.getMessages();
-    setMessages(messages);
-  };
+  const { refetch: updateMessages, isFetching: updateMessagesIsFetching } =
+    useQuery({
+      queryKey: ['updateMessages'],
+      queryFn: async () => {
+        const messages = await api.getMessages();
+        setMessages(messages.reverse());
+        return true;
+      },
+      enabled: true,
+    });
 
-  const handleUpload = async () => {
-    if (file) {
-      const response = await api.createUpload();
-      const imageId = response.id;
-      await api.uploadStorageService(response.presignedURL, file);
-      await api.addMessage(content, imageId);
-      await updateMessages();
-    }
-  };
+  const { refetch: sendMessage, isFetching: sendMessageIsFetching } = useQuery({
+    queryKey: ['sendMessage'],
+    queryFn: async () => {
+      if (selectedFilePath && selectedFile) {
+        const response = await api.createUpload();
+        const imageId = response.id;
+        await api.uploadStorageService(response.presignedURL, selectedFile);
+        await api.addMessage(content, imageId);
+        updateMessages();
+        setContent('');
+        selectedFilePath.current = '';
+        setSelectedFile(undefined);
+      }
+      return true;
+    },
+    enabled: false,
+  });
 
-  useEffect(() => {
-    updateMessages();
-  }, []);
+  const buttonDisabled =
+    updateMessagesIsFetching ||
+    sendMessageIsFetching ||
+    selectedFile === undefined ||
+    selectedFilePath.current === '' ||
+    content === '';
 
   return (
     <>
@@ -44,15 +68,23 @@ const ImagePage = () => {
       </div>
       <div>
         <span>請選擇檔案: </span>
-        <ImageUploader onChange={(e) => setFile(e.target.files?.[0])} />
+        <ImageUploader
+          value={selectedFilePath.current}
+          onChange={(e) => {
+            selectedFilePath.current = e.target.value;
+            setSelectedFile(e.target.files?.[0]);
+          }}
+        />
       </div>
-      <button onClick={handleUpload}>送出</button>
+      <button disabled={buttonDisabled} onClick={() => sendMessage()}>
+        送出
+      </button>
       <hr />
       {messages.map((message, i) => {
         return (
           <div key={i}>
             <p>{message.content}</p>
-            <img
+            <CenterCropped
               width={300}
               height={200}
               src={message.imageURL}
