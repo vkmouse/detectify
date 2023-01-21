f28b8f5bbf5647a4f250a359cd7f2ce27646c12b
vkmouse <knight31241@gmail.com>
2023-01-21 17:23:04 +0800

    feat: update backend cors

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -11,7 +11,7 @@ func CORSMiddleware() func(ctx *gin.Context) {
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", config.AllowOrigin)
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.AbortWithStatus(204)
