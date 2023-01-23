69778eaa78c4e85be7177b951695857c8c8e9d22
vkmouse <knight31241@gmail.com>
2023-01-23 11:36:44 +0800

    feat: modify orm model and add foreign key

--- backend/internal/model/project.go
+++ backend/internal/model/project.go
@@ -6,20 +6,22 @@ import (
 
 type Project struct {
 	gorm.Model
-	Name        string `gorm:"type:varchar(255) NOT NULL;" json:"name"`
-	Description string `gorm:"type:varchar(255) NOT NULL;" json:"description"`
-	UserID      string `gorm:"type:varchar(36) NOT NULL;"`
-	Cover       string `gorm:"type:varchar(255) NOT NULL;" json:"cover"`
+	ID          string `gorm:"primarykey;type:varchar(36) not null;"`
+	Name        string `gorm:"type:varchar(255) not null;" json:"name"`
+	Description string `gorm:"type:varchar(255) not null;" json:"description"`
+	Cover       string `gorm:"type:varchar(255) not null;" json:"cover"`
+	UserID      string
+	User        User
 }
 
 type ProjectCategory struct {
-	gorm.Model
-	Name      string `gorm:"type:varchar(255) NOT NULL;" json:"name"`
-	ProjectID uint   `gorm:"NOT NULL;" json:"projectID"`
+	Name      string `gorm:"primarykey;type:varchar(255) not null;" json:"name"`
+	ProjectID string `gorm:"primarykey;" json:"projectID"`
+	Project   Project
 }
 
 type ProjectImage struct {
-	gorm.Model
-	URL       string `gorm:"type:varchar(255) NOT NULL;" json:"name"`
-	ProjectID uint   `gorm:"NOT NULL;" json:"projectID"`
+	URL       string `gorm:"primarykey;type:varchar(255) not null;" json:"name"`
+	ProjectID string `gorm:"primarykey;" json:"projectID"`
+	Project   Project
 }
--- backend/internal/model/user.go
+++ backend/internal/model/user.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	ID       string `gorm:"primarykey type:varchar(36) NOT NULL;"`
-	Name     string `gorm:"type:varchar(255) NOT NULL;" json:"name"`
-	Email    string `gorm:"type:varchar(255) NOT NULL unique;" json:"email"`
-	Password string `gorm:"type:varchar(72) NOT NULL;" json:"password"`
-	Avatar   string `gorm:"type:varchar(255) NOT NULL;" json:"avatar"`
+	ID       string `gorm:"primarykey;type:varchar(36) not null;"`
+	Name     string `gorm:"type:varchar(255) not null;" json:"name"`
+	Email    string `gorm:"type:varchar(255) not null unique;" json:"email"`
+	Password string `gorm:"type:varchar(72) not null;" json:"password"`
+	Avatar   string `gorm:"type:varchar(255) not null;" json:"avatar"`
 }
