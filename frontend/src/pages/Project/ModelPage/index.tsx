bc46198a660fe2d9eaa2b03f87cef8b35187f63f
vkmouse <knight31241@gmail.com>
2023-02-03 11:17:51 +0800

    feat: add model predict

--- frontend/src/pages/Project/ModelPage/index.tsx
+++ frontend/src/pages/Project/ModelPage/index.tsx
@@ -1,6 +1,9 @@
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import api from '../../../api/api';
+import { PrimaryButton } from '../../../components/Button';
 import ImageCard from '../../../components/ImageCard';
 import { useProjectInfo } from '../../../context/ProjectInfoContext';
+import { InferResponse } from '../../../types/api';
 import {
   CanvasInnerWrapper,
   CanvasWrapper,
@@ -8,40 +11,88 @@ import {
   ImageCardContainer,
 } from './styles';
 
-const ModelPage = () => {
-  const { images } = useProjectInfo();
+type Size = {
+  width: number;
+  height: number;
+};
+
+const Canvas = React.forwardRef<HTMLCanvasElement>(({}, ref) => {
   const divRef = useRef<HTMLDivElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [canvasSize, setCanvasSize] = useState({ width: 0, height: 0 });
+  const [size, setSize] = useState<Size>({ width: 0, height: 0 });
 
   useEffect(() => {
     if (divRef.current) {
-      setCanvasSize({
+      setSize({
         width: divRef.current?.clientWidth,
         height: divRef.current?.clientHeight,
       });
     }
-  }, [divRef.current]);
+  }, [divRef.current?.clientWidth, divRef.current?.clientHeight]);
+
+  return (
+    <div ref={divRef} style={{ width: '100%', height: '100%' }}>
+      <canvas ref={ref} {...size} />
+    </div>
+  );
+});
+
+Canvas.displayName = 'Canvas';
+
+const ModelPage = () => {
+  const { images } = useProjectInfo();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const currImageURL = useRef('');
+  const [rect, setRect] = useState({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    ratio: 1.0,
+  });
+
+  const updateRect = (canvasSize: Size, size: Size) => {
+    let { width, height } = size;
+    let ratio = 1.0;
+    if (canvasSize.width < width || canvasSize.height < height) {
+      ratio = Math.min(canvasSize.width / width, canvasSize.height / height);
+      width = width * ratio;
+      height = height * ratio;
+    }
+    const x = (canvasSize.width - width) / 2;
+    const y = (canvasSize.height - height) / 2;
+    setRect({ x, y, width, height, ratio });
+    return { x, y, width, height };
+  };
 
   const drawCanvas = (image: HTMLImageElement) => {
-    let imgWidth = image.naturalWidth;
-    let imgHeight = image.naturalHeight;
     const canvas = canvasRef.current;
     const context = canvas?.getContext('2d');
-
-    if (canvas && context && imgWidth && imgHeight) {
+    if (canvas && context) {
+      const canvasSize = { width: canvas.width, height: canvas.height };
+      const size = { width: image.naturalWidth, height: image.naturalHeight };
+      const { x, y, width, height } = updateRect(canvasSize, size);
       context.clearRect(0, 0, canvas.width, canvas.height);
-      if (canvas.width < imgWidth || canvas.height < imgHeight) {
-        const ratio = Math.min(
-          canvas.width / imgWidth,
-          canvas.height / imgHeight
-        );
-        imgWidth = imgWidth * ratio;
-        imgHeight = imgHeight * ratio;
+      context.drawImage(image, x, y, width, height);
+    }
+  };
+
+  const drawBoundingBoxes = (boundingBoxes: InferResponse[]) => {
+    if (boundingBoxes && currImageURL.current) {
+      console.log(boundingBoxes);
+      const canvas = canvasRef.current;
+      const context = canvas?.getContext('2d');
+      if (canvas && context) {
+        for (const box of boundingBoxes) {
+          const x = rect.x + box.x * rect.ratio;
+          const y = rect.y + box.y * rect.ratio;
+          const width = box.width * rect.ratio;
+          const height = box.height * rect.ratio;
+          context.strokeStyle = '#FF0000';
+          context.beginPath();
+          context.rect(x, y, width, height);
+          context.stroke();
+        }
       }
-      const x = (canvas.width - imgWidth) / 2;
-      const y = (canvas.height - imgHeight) / 2;
-      context.drawImage(image, x, y, imgWidth, imgHeight);
     }
   };
 
@@ -52,7 +103,10 @@ const ModelPage = () => {
           return (
             <ImageCardContainer key={i}>
               <ImageCard
-                onClick={drawCanvas}
+                onClick={(img) => {
+                  currImageURL.current = img.src;
+                  drawCanvas(img);
+                }}
                 src={p.imageURL}
                 title={p.filename}
               />
@@ -61,14 +115,28 @@ const ModelPage = () => {
         })}
       </HotizentalImageList>
       <CanvasWrapper>
-        <CanvasInnerWrapper onResize={() => console.log('fsdf')} ref={divRef}>
-          <canvas
-            width={canvasSize.width}
-            height={canvasSize.height}
-            ref={canvasRef}
-          />
+        <CanvasInnerWrapper>
+          <Canvas ref={canvasRef} />
         </CanvasInnerWrapper>
       </CanvasWrapper>
+      <PrimaryButton
+        onClick={() => {
+          api
+            .infer({
+              modelURL:
+                'https://pub-524340b28b994541ba4d1f39e64d2b3d.r2.dev/ssd320.zip',
+              imageURL: currImageURL.current,
+              threshold: 0.5,
+              width: 320,
+              height: 320,
+            })
+            .then((data) => {
+              drawBoundingBoxes(data);
+            });
+        }}
+      >
+        Detect
+      </PrimaryButton>
     </>
   );
 };
