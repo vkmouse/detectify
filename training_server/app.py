441e0eee338c32fc4a2fc81d97416afa917354f7
vkmouse <knight31241@gmail.com>
2023-02-08 19:23:20 +0800

    feat: update response of training server

--- training_server/app.py
+++ training_server/app.py
@@ -4,8 +4,8 @@ from training_server import route
 
 app = Flask(__name__)
 
-app.register_blueprint(route.train_bp)
-
+app.register_blueprint(route.model_bp)
+app.register_blueprint(route.server_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9000, debug=True)
