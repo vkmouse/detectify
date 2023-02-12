a71da22dc1d0aab76a3611a0fe5dbb559948cdc8
vkmouse <knight31241@gmail.com>
2023-02-12 11:43:10 +0800

    feat: add memory for inference engine

--- inference_server/inference_server/controller.py
+++ inference_server/inference_server/controller.py
@@ -1,7 +1,18 @@
 from inference_server.inferencer import BaseInferencer
 
 
+memory_inferencer = {
+    'model_url': '',
+    "engine": None,
+}
+
+
 def infer(model_url, image_url, threshold):
-    engine = BaseInferencer(model_url)
+    if memory_inferencer['model_url'] == model_url:
+        engine = memory_inferencer['engine']
+    else:
+        engine = BaseInferencer(model_url)
     engine.infer(image_url)
+    memory_inferencer['model_url'] = model_url
+    memory_inferencer['engine'] = engine
     return engine.parser_results(threshold)
