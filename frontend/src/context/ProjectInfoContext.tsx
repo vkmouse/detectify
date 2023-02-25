327f12418f32421acb2623661b8ee90c4c391919
vkmouse <knight31241@gmail.com>
2023-02-25 20:43:00 +0800

    feat: preload for loading image and project

--- frontend/src/context/ProjectInfoContext.tsx
+++ frontend/src/context/ProjectInfoContext.tsx
@@ -1,11 +1,5 @@
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { BatchUploadResponse } from '../types/api';
 import api from '../api/api';
 
@@ -14,7 +8,9 @@ type State = {
   name: string | null;
   images: BatchUploadResponse[];
   irModel: string;
+  isProjectImagesLoading: boolean;
   exportedModel: string;
+  removeProjectImage: (imageId: string) => void;
 };
 
 const initialState: State = {
@@ -22,53 +18,67 @@ const initialState: State = {
   name: null,
   images: [],
   irModel: '',
+  isProjectImagesLoading: false,
   exportedModel: '',
+  removeProjectImage: () => void 0,
 };
 
 const ProjectInfoContext = createContext<State>(initialState);
 
-const useProjectImages = (projectId: string) => {
+const ProjectInfoProvider = ({ children }: { children: ReactNode }) => {
+  const { pathname } = window.location;
+  const subs = pathname.split('/');
+  const id = subs[subs.findIndex((p) => p === 'project') + 1];
+  const queryClient = useQueryClient();
+  const [name, setName] = useState('');
+  const [irModel, setIRModel] = useState('');
+  const [exportedModel, setExportedModel] = useState('');
   const [images, setImages] = useState<BatchUploadResponse[]>([]);
 
   useQuery({
+    queryKey: ['projectInfo'],
+    queryFn: () => {
+      return api.getProject(id);
+    },
+    onSuccess: (data) => {
+      setName(data.projectName);
+      setIRModel(data.irModel);
+      setExportedModel(data.exportedModel);
+    },
+  });
+
+  const { isFetching } = useQuery({
     queryKey: ['projectImages'],
     queryFn: async () => {
-      return await api.getProjectImages(projectId);
+      return await api.getProjectImages(id);
     },
     onSuccess: (data) => {
       setImages(data);
     },
   });
 
-  return { images };
-};
-
-const useProjectName = (projectId: string) => {
-  const [name, setName] = useState('');
-  const [irModel, setIRModel] = useState('');
-  const [exportedModel, setExportedModel] = useState('');
-
-  useEffect(() => {
-    api.getProject(projectId).then((data) => {
-      setName(data.projectName);
-      setIRModel(data.irModel);
-      setExportedModel(data.exportedModel);
-    });
-  }, []);
-
-  return { irModel, exportedModel, name };
-};
-
-const ProjectInfoProvider = ({ children }: { children: ReactNode }) => {
-  const { pathname } = window.location;
-  const subs = pathname.split('/');
-  const id = subs[subs.findIndex((p) => p === 'project') + 1];
-  const { name, irModel, exportedModel } = useProjectName(id);
-  const { images } = useProjectImages(id);
+  const { isLoading, mutate } = useMutation<string, Error, string>({
+    mutationFn: async (filename) => {
+      await api.removeProjectImage(id, filename);
+      return Promise.resolve(filename);
+    },
+    onSuccess: (filename) => {
+      queryClient.invalidateQueries(['projectImages']);
+      setImages((prev) => prev.filter((p) => p.filename !== filename));
+    },
+  });
 
   return (
     <ProjectInfoContext.Provider
-      value={{ id, name, images, irModel, exportedModel }}
+      value={{
+        id,
+        name,
+        images,
+        irModel,
+        isProjectImagesLoading: isFetching || isLoading,
+        exportedModel,
+        removeProjectImage: mutate,
+      }}
     >
       {children}
     </ProjectInfoContext.Provider>
