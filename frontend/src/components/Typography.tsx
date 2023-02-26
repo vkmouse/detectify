9985c5350eb4c5efd6924773b739c5a6180d4a57
vkmouse <knight31241@gmail.com>
2023-02-26 16:30:58 +0800

    feat: add home page

--- frontend/src/components/Typography.tsx
+++ frontend/src/components/Typography.tsx
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
 
-const H3 = styled.h3`
-  display: block;
-  font-size: 1.44375rem;
-  margin: 0 0 8px 0;
-  font-weight: bold;
+const H1 = styled.h1`
+  font-size: 1.65rem;
+  margin-bottom: 0.5rem;
+  margin-top: 0;
+  font-weight: 500;
+  line-height: 1.2;
 `;
 
-export { H3 };
+const H3 = styled.h3`
+  font-size: 1.44375rem;
+  margin: 0 0 8px 0;
+  font-weight: 500;
+`;
+
+export { H1, H3 };
